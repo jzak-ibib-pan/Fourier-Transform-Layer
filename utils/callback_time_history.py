d22e41befbadf42feea493b79f7b0945d2bbaa73
zigzaq <jjacobzak@gmail.com>
2022-02-04 11:39:32 +0100

    Initial utils commit.

---
+++ utils/callback_time_history.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.callbacks import Callback
+from time import time
+
+
+class TimeHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time() - self.epoch_time_start)
+
+
+if __name__ == '__main__':
+    print(0)
