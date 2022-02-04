f6971eebbb30d3912d4b4cfec31810dd34afb712
zigzaq <jjacobzak@gmail.com>
2022-02-04 11:40:41 +0100

    Updated time history callback to contain init function

--- utils/callback_time_history.py
+++ utils/callback_time_history.py
@@ -3,14 +3,18 @@ from time import time
 
 
 class TimeHistory(Callback):
+    def __init__(self):
+        self.times = []
+        self._epoch_time_start = 0
+
     def on_train_begin(self, logs={}):
         self.times = []
 
     def on_epoch_begin(self, batch, logs={}):
-        self.epoch_time_start = time()
+        self._epoch_time_start = time()
 
     def on_epoch_end(self, batch, logs={}):
-        self.times.append(time() - self.epoch_time_start)
+        self.times.append(time() - self._epoch_time_start)
 
 
 if __name__ == '__main__':
