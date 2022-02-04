b24912691bd6a086b5a04983b5a53781d74e2155
zigzaq <jjacobzak@gmail.com>
2022-02-04 15:06:08 +0100

    Removed keras Model inheritance.

--- utils/model_builder.py
+++ utils/model_builder.py
@@ -1,32 +1,31 @@
-from tensorflow.keras.models import Model
 from contextlib import redirect_stdout
 from os import listdir
 from os.path import join
 from datetime import datetime as dt
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.applications.mobilenet import MobileNet
 
 
-class ModelBuilder(Model):
+class ModelBuilder():
     def __init__(self):
-        super(ModelBuilder, self).__init__(self)
-
-    def call(self, inputs, training=None, mask=None):
-        pass
-
-    def get_config(self):
-        pass
+        self.model = self.build((32, 32, 1))
 
     def save_model_info(self, filename, extension='', filepath=''):
-        filename_expanded = self._expand_filename(filename, filepath)
-        format_used = extension
-        if len(format_used) < 1:
-            format_used = '.txt'
-        if '.' not in format_used:
-            format_used = '.' + format_used
-        with open(join(filepath, filename_expanded + format_used), 'w') as fil:
-            with redirect_stdout(fil):
-                print('Over 9000!')
-                # self.summary()
-        return filename_expanded
+            filename_expanded = self._expand_filename(filename, filepath)
+            format_used = extension
+            if len(format_used) < 1:
+                format_used = '.txt'
+            if '.' not in format_used:
+                format_used = '.' + format_used
+            with open(join(filepath, filename_expanded + format_used), 'w') as fil:
+                with redirect_stdout(fil):
+                    self.model.summary()
+            return filename_expanded
+
+    @staticmethod
+    def build(input_shape, noof_classes=1):
+        return []
 
     @staticmethod
     def _expand_filename(filename, filepath=''):
@@ -40,6 +39,21 @@ class ModelBuilder(Model):
         return filename_expanded
 
 
+class CNNBuilder(ModelBuilder):
+    def __init__(self):
+        super(CNNBuilder, self).__init__()
+
+    @staticmethod
+    def build(input_shape, noof_classes=1):
+        backbone = MobileNet(input_shape, weights=None, include_top=False)
+        architecture = backbone.output
+        arch = backbone.output
+        # Classify
+        flat = Flatten()(arch)
+        out = Dense(noof_classes, activation=act)(flat)
+        return Model(inputs=[backbone.input], outputs=[out])
+
+
 if __name__ == '__main__':
-    model = ModelBuilder()
-    model.save_model_info('test', '.txt', '../test')
+    builder = CNNBuilder()
+    builder.save_model_info('test', '.txt', '../test')
