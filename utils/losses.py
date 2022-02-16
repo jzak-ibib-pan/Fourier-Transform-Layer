69845391b2231b8502d97d39cee3f1842754baa4
zigzaq <jjacobzak@gmail.com>
2022-02-16 14:13:40 +0100

    TF implementation of SSIM in place of skimage.

--- utils/losses.py
+++ utils/losses.py
@@ -1,9 +1,4 @@
-from skimage.metrics import structural_similarity
-from cv2 import resize
-from numpy import expand_dims
+import tensorflow as tf
 
 def ssim(y_true, y_pred):
-    _true = resize(y_true, (64, 64))
-    _true = expand_dims(_true, axis=-1)
-    index = structural_similarity(y_true, y_pred, gaussian_weights=True, sigma=1.5,  use_sample_covariance=False)
-    return 1 - index
+    return 1 - tf.image.ssim(y_true, y_pred, max_val=255)
