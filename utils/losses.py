06fd9560bb7bd4167a38120f39604c725c6f0636
zigzaq <jjacobzak@gmail.com>
2022-02-16 14:18:43 +0100

    Implementation of SSIM. Returns 0 if images are the same, 1 - completely different. Added casting to remove potential bugs.

--- utils/losses.py
+++ utils/losses.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 
+
 def ssim(y_true, y_pred):
-    return 1 - tf.image.ssim(y_true, y_pred, max_val=255)
+    # TF SSIM returns value range (-1; 1]
+    result = tf.image.ssim(y_true, y_pred, max_val=255)
+    # range (0; 2]
+    result = tf.add(result, 1)
+    # range (0; 1]
+    result = tf.divide(result, 2)
+    # flip the values to minimize loss
+    return tf.subtract(tf.cast(1, dtype=tf.float32), tf.cast(result, dtype=tf.float32))
