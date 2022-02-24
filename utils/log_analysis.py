c03d363a54bcbdf7b0d6c08ce20bc6d75e0243cc
zigzaq <jjacobzak@gmail.com>
2022-02-24 10:05:21 +0100

    Moved log analysis back to utils.

--- utils/log_analysis.py
+++ utils/log_analysis.py
@@ -1,7 +1,23 @@
-def analyze():
+def analyze(lines, monitor=[]):
+    for line in lines:
+        for monit in monitor:
+            if monit not in line:
+                continue
+            print(line)
+    eva_index = 0
+    while 'Evaluation' not in lines[eva_index] and eva_index < len(lines):
+        eva_index += 1
+    print(lines[eva_index + 1 : eva_index + 3])
 
-    print(0)
+
+def main():
+    monitor = ['ftl-activation', 'Dataset']
+    filename = 'mnist_abs-calc_True_2022-02-23_15_37_52_007.txt'
+    filepath = '../experiments/hyperparameter_optimization/results/try_00/'
+    with open(filepath + filename, 'r') as fil:
+        lines = fil.readlines()
+    analyze(lines, monitor)
 
 
 if __name__ == '__main__':
-    print(analyze())
+    main()
