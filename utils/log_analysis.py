ca7cb9b2b33f989bdfeefeb0ee982f627399e89c
zigzaq <jjacobzak@gmail.com>
2022-02-24 11:26:10 +0100

    Log analysis for hyperparameter optimization.

--- utils/log_analysis.py
+++ utils/log_analysis.py
@@ -1,23 +1,106 @@
+from os import listdir
+from pandas import DataFrame as df
+from numpy import mean, std, median
+
+
+STANDARD_KEYS = ['ftl-activation',
+                 'ftl-calculate_abs',
+                 'input_shape',
+                 ]
+
+
+def clean_eva_line(line):
+    # analyze evaluation lines
+    # -1 - \n sign
+    splits = line.replace(' ', '').split('||')[:-1]
+    splits[0] = splits[0].split('--')[1]
+    return splits
+
+
+def extract_from_line(line):
+    _line = line.strip().replace(' ', '')
+    if 'Dataset' in line:
+        # [-1] - dataset, [-1] - dot at the end
+        return _line.split(':')[-1][:-1]
+    if any(key in line for key in STANDARD_KEYS):
+        return _line.split('-')[-1]
+    return None
+
+
 def analyze(lines, monitor=[]):
-    for line in lines:
-        for monit in monitor:
-            if monit not in line:
+    result = {'Evaluation': {}}
+    for monit in monitor:
+        result.update({monit: None})
+    # find evaluation lines
+    index = 0
+    while 'Evaluation' not in lines[index] and index < len(lines):
+        index += 1
+    keys = clean_eva_line(lines[index + 1])
+    values = clean_eva_line(lines[index + 2])
+    for key, value in zip(keys, values):
+        result['Evaluation'].update({key: float(value)})
+    for monit in monitor:
+        index = 0
+        while monit not in lines[index] and index < len(lines):
+            index += 1
+        if index == len(lines):
+            continue
+        monitored = lines[index]
+        result[monit] = extract_from_line(monitored)
+    return result
+
+
+def main(use_abs=False, calculate='mean'):
+    chosen_metric = 'cat_acc'
+    frame = {'None': {},
+             'tanh': {},
+             'selu': {},
+             'relu': {},
+             }
+    # set zeros for mean calculation
+    for key in frame.keys():
+        for dset in ['cifar10', 'fmnist', 'mnist']:
+            frame[key].update({dset: []})
+    monitor = ['input_shape', 'ftl-activation', 'ftl-calculate_abs', 'Dataset']
+    for tries in range(10):
+        filepath = f'../experiments/hyperparameter_optimization/results/try_0{tries}/'
+        for filename in listdir(filepath):
+            if filename == 'checkpoints':
                 continue
-            print(line)
-    eva_index = 0
-    while 'Evaluation' not in lines[eva_index] and eva_index < len(lines):
-        eva_index += 1
-    print(lines[eva_index + 1 : eva_index + 3])
-
-
-def main():
-    monitor = ['ftl-activation', 'Dataset']
-    filename = 'mnist_abs-calc_True_2022-02-23_15_37_52_007.txt'
-    filepath = '../experiments/hyperparameter_optimization/results/try_00/'
-    with open(filepath + filename, 'r') as fil:
-        lines = fil.readlines()
-    analyze(lines, monitor)
+            with open(filepath + filename, 'r') as fil:
+                lines = fil.readlines()
+            result = analyze(lines, monitor)
+            # remember to remove spaces - '' work, str - does not
+            if result['ftl-calculate_abs'] == str(not use_abs) or result['input_shape'] == '(32,32,3)':
+                continue
+            frame[result['ftl-activation']][result['Dataset']].append(result['Evaluation'][chosen_metric])
+    result = frame.copy()
+    for key in frame.keys():
+        for dset in ['cifar10', 'fmnist', 'mnist']:
+            if calculate == 'mean':
+                result[key][dset] = mean(frame[key][dset])
+                continue
+            if calculate == 'std':
+                result[key][dset] = std(frame[key][dset])
+                continue
+            if calculate == 'median':
+                result[key][dset] = median(frame[key][dset])
+                continue
+    return df.from_dict(data=result)
 
 
 if __name__ == '__main__':
-    main()
+    frames = []
+    for calc in ['mean', 'std', 'median']:
+        frames.append(main(calculate=calc))
+    frame_mean = frames[0].to_numpy()
+    frame_std = frames[1].to_numpy()
+    for line_id in range(frame_mean.shape[0]):
+        txt = ''
+        me = frame_mean[line_id]
+        st = frame_std[line_id]
+        for value_me, value_st in zip(me, st):
+            txt += f'{value_me:.3f} $\pm$ {value_st:.3f} & '
+        txt = txt[:-2]
+        txt += '\ \\'.replace(' ', '')
+        print(txt)
