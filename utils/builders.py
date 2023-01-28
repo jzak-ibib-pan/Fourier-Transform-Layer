1285ea1bde3cdff5cbbfb78ed7e14da25909ad83
zigzaq <jjacobzak@gmail.com>
2023-01-28 13:17:17 +0100

    Removed Unet as the concatenate layer causes problems with Builders.

--- utils/builders.py
+++ utils/builders.py
@@ -5,7 +5,6 @@ from datetime import datetime as dt
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Flatten, Dense, BatchNormalization, Input, Conv2D, Concatenate, Conv2DTranspose
 from tensorflow.keras.layers import Add, ZeroPadding2D, Activation, MaxPooling2D, AveragePooling2D, ReLU, DepthwiseConv2D
-from tensorflow.keras.layers import Dropout, concatenate, UpSampling2D
 import tensorflow.keras.applications as apps
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.metrics import Accuracy, CategoricalAccuracy, TopKCategoricalAccuracy, AUC
@@ -24,104 +23,6 @@ from utils.callbacks import TimeHistory, EarlyStopOnBaseline
 from utils.sampling import DIRECTIONS, sampling_calculation
 from utils.losses import ssim
 from types import GeneratorType
-
-
-def unet_modified(input_shape,
-                  weights,
-                  include_top=False,
-                  **kwargs):
-    # updated merge to to concacenate to remove warnings
-    # updated Model
-    # increased number of filters of up8 and beyond
-    # added BatchNorm after every upsampling (ups)
-    inputs = Input(input_shape)
-
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    up6 = BatchNormalization()(up6)
-    ###########################################################################
-    # JZ update?
-    merge6 = concatenate([drop4, up6], axis=3)
-    #merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
-    ###########################################################################
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    up7 = BatchNormalization()(up7)
-    ###########################################################################
-    # JZ update?
-    merge7 = concatenate([conv3, up7], axis=3)
-    #merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
-    ###########################################################################
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-    ###########################################################################
-    # JZ update?
-    #up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    up8 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    up8 = BatchNormalization()(up8)
-    merge8 = concatenate([conv2, up8], axis=3)
-    #merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
-    #conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(192, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    #conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-    conv8 = Conv2D(192, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-    #up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    up9 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    up9 = BatchNormalization()(up9)
-    # JZ update?
-    merge9 = concatenate([conv1, up9], axis=3)
-    #merge9 = merge([conv1,up9], mode = 'concat', concat_axis = 3)
-    #conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(96, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    #conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(96, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    #conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(24, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-
-    return Model(inputs, conv9)
-
-    # if categories == 1:
-    #     conv10 = Conv2D(categories, 1, activation = 'sigmoid', strides=(1, 1))(conv9)
-    # else:
-    #     conv10 = Conv2D(categories, 1, activation = 'softmax', strides=(1, 1))(conv9)
-    #
-    # #conv10 = Conv2D(categories, 1, activation = 'sigmoid', strides=(1, 1))(conv9)
-    # model = Model(inputs, conv10)
-    # ###########################################################################
-    # adam = Adam(lr=1e-4, decay=1e-6)
-    #
-    # if categories == 1:
-    #     model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
-    # else:
-    #     model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-    #
-    # #model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
-    # #model.summary()
-    #
-    # if pretrained_weights:
-    #     model.load_weights(pretrained_weights)
-    #
-    # return model
 
 
 # TODO: error messages
@@ -883,8 +784,6 @@ class CNNBuilder(ModelBuilder):
                 return None
         elif 'inception' in model_type_low:
             _backbone = apps.InceptionV3
-        elif 'unet' in model_type_low:
-            _backbone = unet_modified
         if not _backbone:
             return None
         backbone = _backbone(input_shape=input_shape, weights=weights, include_top=False)
@@ -920,6 +819,8 @@ class CNNBuilder(ModelBuilder):
             elif '101' in model_type_low:
                 # load resnet101
                 _backbone = apps.resnet_v2.ResNet101V2
+        elif 'unet' in model_type_low:
+            _backbone = unet_modified
         if not _backbone:
             return None
         backbone = _backbone(input_shape=input_shape, weights=weights, include_top=False)
